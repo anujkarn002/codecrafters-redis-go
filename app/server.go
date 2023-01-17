d19610c5b5597f18036a82a8a5d5259145b13928
anujkarn002 <anujkarn002@gmail.com>
2023-01-17 15:02:28 +0530

    pass stage 3

--- app/server.go
+++ app/server.go
@@ -7,6 +7,11 @@ import (
 	"os"
 )
 
+const (
+	MAXBUFFER = 5000
+	PONG      = "+PONG\r\n"
+)
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -18,18 +23,32 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+	defer l.Close()
 	for {
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go func(conn net.Conn) {
-			_, err = conn.Write([]byte("+PONG\r\n"))
-			if err != nil {
-				err = conn.Close()
-				os.Exit(1)
-			}
-		}(conn)
+		fmt.Println("Server Connected")
+		go handleConn(conn)
 	}
+}
+
+func handleConn(conn net.Conn) {
+	buffer := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		fmt.Printf("Received %v\n", string(buffer[0:n]))
+		handlePing(conn)
+	}
+}
+
+func handlePing(conn net.Conn) string {
+	conn.Write([]byte(PONG))
+	return PONG
 }
