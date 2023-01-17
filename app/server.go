c7cb76fc03712a1aae518043e25472c67130ac6c
anujkarn002 <anujkarn002@gmail.com>
2023-01-17 12:54:16 +0530

    add simple echo in stage 2

--- app/server.go
+++ app/server.go
@@ -18,9 +18,18 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go func(conn net.Conn) {
+			_, err = conn.Write([]byte("+PONG\r\n"))
+			if err != nil {
+				err = conn.Close()
+				os.Exit(1)
+			}
+		}(conn)
 	}
 }
